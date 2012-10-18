3144a9f48fc45ef99d3439fb0b8fca3bd222cc68
Christopher Gandrud <christopher.gandrud@gmail.com>
2012-10-18 13:58:30 +0900

    csv data

---
+++ Analysis/leg_violence_map.R
@@ -0,0 +1,27 @@
+library(foreign)
+library(googleVis)
+library(gregmisc)
+library(plyr)
+
+## Load data
+leg.raw <- read.csv("/Users/christophergandrud/Dropbox/Leg_Violence/Data/Legislative_Violence_Research_stata_ready.csv")
+
+# Get frequency count
+  incidence <- count(leg.raw, c("violence", "Country"))
+  incidence <- subset(incidence, violence == 1)
+  incidence <- incidence[,2:3]
+
+# Clean up data
+  incidence$Country <- gsub("Korea South", "South Korea", incidence$Country)
+  incidence <- rename.vars(incidence, from ="freq", to = "Leg Brawl", info = FALSE)
+
+# Make Google Map
+violence.map <- gvisGeoMap(incidence, locationvar = "Country", numvar = "Leg Brawl", 
+                           options = list(
+                             colors = "[0xEDDD00, 0xED6700, 0xA04400]"
+                             )
+                           )
+
+plot(violence.map)
+
+print(violence.map, "chart", file = "/Users/christophergandrud/Dropbox/Public/Graphs/leg.violence.map.html")
