05798603795fc4916260c6645e9a4e6bd056a5bc
Christopher Gandrud <christopher.gandrud@gmail.com>
2013-06-29 17:51:11 +0900

    majority conclusions

--- Analysis/expectedValueGraphs2.R
+++ Analysis/expectedValueGraphs2.R
@@ -14,6 +14,42 @@ library(plyr)
     dem.r <- seq(from = 0, to = 85, by = 2)
     maj.r <- seq(from = 20, to = 100, by = 2)
 
+## Disporportionality < 5 Dummy
+## Set fitted values
+DN2.HighProp <- setx(DN2, HighProp = HighProp.r)
+
+# Simulate quantities of interest
+DN2.HighPropSim <- sim(DN2, x = DN2.HighProp)
+
+# Extract expected values from simulations
+DN2.HighProp.e <- data.frame(simulation.matrix(DN2.HighPropSim, "Expected Values: E(Y|X)"))
+DN2.HighProp.e <- melt(DN2.HighProp.e, measure = 1:2)
+
+# Remove "X" from variable
+DN2.HighProp.e$variable <- as.numeric(gsub("X", "", DN2.HighProp.e$variable))
+
+# Remove values outside of the 2.5% and 97.5% quantiles
+# Find 2.5% and 97.5% quantiles for HRCC
+DN2.HighProp.ePer <- ddply(DN2.HighProp.e, .(variable), transform, 
+                           Lower = value < quantile(value, c(0.025)))
+
+DN2.HighProp.ePer <- ddply(DN2.HighProp.ePer, .(variable), transform, 
+                           Upper = value > quantile(value, c(0.975)))
+
+# Remove variables outside of the middle 95%
+DN2.HighProp.ePer <- subset(DN2.HighProp.ePer, Lower == FALSE & Upper == FALSE)
+
+# Plot
+DN2.HighProp.p <- ggplot(DN2.HighProp.ePer, aes(variable, value)) +
+  geom_point(shape = 21, color = "gray30", alpha = I(0.05)) +
+  stat_smooth(method = "lm", se = FALSE) +
+  scale_x_reverse(breaks = c(1, 2), labels = c("Higher", "Very Low")) +
+  scale_y_continuous(breaks = c(0, 0.02, 0.05, 0.1, 0.125), 
+                     labels = c("0", "0.02", "0.05", "0.1", "0.125"),
+                     limits = c(0, 0.125)) +
+  xlab("\nDisproportionality") + ylab("") +
+  theme_bw(base_size = 12)
+
 ## Age of Democracy
     # Set fitted values  
     DN2.DemAge <-setx(DN2, DemAge = dem.r)
@@ -48,44 +84,10 @@ library(plyr)
                         stat_smooth() +
                         scale_x_continuous(breaks = c(0, 21, 51, 85), 
                                            labels = c("0", "20", "50", "85")) +
-                        scale_y_continuous(breaks = c(0, 0.02, 0.05, 0.08), 
-                                           limits = c(0, 0.08)) +
+                        scale_y_continuous(breaks = c(0, 0.02, 0.05, 0.1, 0.125), 
+                                           labels = c("", "", "", "", ""), 
+                                           limits = c(0, 0.125)) +
                         xlab("\nAge of Democracy") + ylab("") +
-                        theme_bw(base_size = 12)
-
-## Disporportionality < 5 Dummy
-    ## Set fitted values
-    DN2.HighProp <- setx(DN2, HighProp = HighProp.r)
-
-    # Simulate quantities of interest
-    DN2.HighPropSim <- sim(DN2, x = DN2.HighProp)
-
-    # Extract expected values from simulations
-    DN2.HighProp.e <- data.frame(simulation.matrix(DN2.HighPropSim, "Expected Values: E(Y|X)"))
-    DN2.HighProp.e <- melt(DN2.HighProp.e, measure = 1:2)
-
-    # Remove "X" from variable
-    DN2.HighProp.e$variable <- as.numeric(gsub("X", "", DN2.HighProp.e$variable))
-
-    # Remove values outside of the 2.5% and 97.5% quantiles
-    # Find 2.5% and 97.5% quantiles for HRCC
-    DN2.HighProp.ePer <- ddply(DN2.HighProp.e, .(variable), transform, 
-                         Lower = value < quantile(value, c(0.025)))
-
-    DN2.HighProp.ePer <- ddply(DN2.HighProp.ePer, .(variable), transform, 
-                         Upper = value > quantile(value, c(0.975)))
-
-    # Remove variables outside of the middle 95%
-    DN2.HighProp.ePer <- subset(DN2.HighProp.ePer, Lower == FALSE & Upper == FALSE)
-
-    # Plot
-    DN2.HighProp.p <- ggplot(DN2.HighProp.ePer, aes(variable, value)) +
-                        geom_point(shape = 21, color = "gray30", alpha = I(0.05)) +
-                        stat_smooth(method = "lm", se = FALSE) +
-                        scale_x_reverse(breaks = c(1, 2), labels = c("Higher", "Very Low")) +
-                        scale_y_continuous(breaks = c(0, 0.02, 0.05, 0.08), 
-                                           labels = c("", "", "", ""), limits = c(0, 0.08)) +
-                        xlab("\nDisproportionality") + ylab("") +
                         theme_bw(base_size = 12)
 ## Majority
     # Set fitted values
@@ -118,9 +120,9 @@ library(plyr)
                         stat_smooth(se = FALSE) +
                         scale_x_continuous(breaks = c(1, 11, 21, 31, 41),
                                            labels = c(20, 40, 60, 80, 100)) +
-                        scale_y_continuous(breaks = c(0, 0.02, 0.05, 0.08), 
-                                           labels = c("", "", "", ""), 
-                                           limits = c(0, 0.08)) +
+                        scale_y_continuous(breaks = c(0, 0.02, 0.05, 0.1, 0.125), 
+                                          labels = c("", "", "", "", ""), 
+                                          limits = c(0, 0.125)) +
                         xlab("\nGovernment Majority") + ylab("") +
                         theme_bw()
     
