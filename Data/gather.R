68497dc5fcb18453e3dbccaca7bd3ed7f5a3f073
christophergandrud <christopher.gandrud@gmail.com>
2015-03-18 11:44:06 +0100

    limited draft new data

--- Data/gather.R
+++ Data/gather.R
@@ -1,7 +1,7 @@
 # ---------------------------------------------------------------------------- #
 # Gather/Clean/Merge Data for Two Sword Lengths Apart
 # Christopher Gandrud
-# 17 March 2015
+# 18 March 2015
 # MIT License
 # ---------------------------------------------------------------------------- #
 
@@ -34,27 +34,92 @@ wdi <- WDI(indicator = indicators_wdi, start = 1975, end = 2014, extra = T) %>%
         filter(region != 'Aggregates') %>%
         select(iso2c, year, gini, gdp_per_capita, murder_rate)
 
+# GDP to thousands of dollars
+wdi$gdp_per_capita <- wdi$gdp_per_capita / 1000
+
+# Assume constant gini
+wdi <- wdi %>% arrange(iso2c, year) %>% group_by(iso2c) %>%
+        mutate(gini = FillDown(Var = gini)) %>%
+        as.data.frame
+
 
 #### --------------- Age of Democracy ---------------------------------------- #
 polity <- PolityGet(url = 'http://www.systemicpeace.org/inscr/p4v2013.sav') %>%
             arrange(iso2c, year)
+
 # Create democratic age variable
 polity$democracy <- 0
 polity$democracy[polity$polity2 > 5] <- 1
 
-cum_nozero <- function(data, x){
-    data$cum <- 0
-    for (i in 1:nrow(data)){
-        if (i == 1) data[1, 'cum'] <- data[1, x]
-        else if (i > 1) {
-            if (data[i-1, x] == 0 & data[i, x] > 0) data[i, 'cum'] <- 1
-            else if (data[i-1, x] > 0) data[i, 'cum'] <- data[i-1, 'cum'] + 1
+cum_nozero <- function(x){
+    polity <- as.data.frame(polity)
+    comb <- vector()
+    for (u in unique(polity$iso2c)){
+        temp <- subset(polity, iso2c == u)
+        temp_cum <- rep(0, nrow(temp))
+    
+        for (i in 1:nrow(temp)){
+            if (i == 1) temp_cum[1] <- temp[1, x]
+            else if (i > 1) {
+                if (temp[i-1, x] == 0 & temp[i, x] > 0) {
+                    temp_cum[i] <- 1
+                }
+                else if (temp[i-1, x] > 0){
+                     temp_cum[i] <- temp_cum[i-1] + 1
+                }
+            }
         }
+        comb <- c(comb, temp_cum)
     }
-    return(data)
+    return(comb)
 }
 
-test = cum_nozero(polity, x = 'democracy')
+polity$dem_age <- cum_nozero(x = 'democracy')
 
-polity <- polity %>% select(iso2c, year, polity2, durable)
+polity <- polity %>% select(iso2c, year, polity2, dem_age, durable)
 
+#### ---------------------- Database of Political Institutions --------------- #
+dpi <- DpiGet() %>% select(iso2c, year, maj, govfrac, allhouse, pr, liec)
+
+for (i in names(dpi)) dpi[, i][dpi[, i] == -999] <- NA
+
+# Convert maj from a proportion to a percent
+dpi$maj <- dpi$maj * 100
+
+#### ----------------- Dispoportionality ------------------------------------- #
+disprop <- import('http://bit.ly/Ss6zDO', format = 'csv') %>% 
+            select(iso2c, year, disproportionality) %>% filter(year >= 1980) %>%
+            filter(year <- 2012)
+
+# Create disproportionality threshold variable where 1 < 6
+disprop$high_prop <- 0
+disprop$high_prop[disprop$disproportionality > 6] <- 1
+
+#### Only countries with elected legislatures
+dpi <- dpi %>% filter(liec > 5)
+
+#### ----------------- Merge together ---------------------------------------- #
+## Merge dpi with disproportionality
+comb <- merge(dpi, disprop, by = c('iso2c', 'year'), all = T) %>%
+        arrange(iso2c, year)
+
+# Fill in disproportionality between election years
+comb <- comb %>% group_by(iso2c) %>% 
+            mutate(disproportionality = FillDown(Var = disproportionality))
+comb <- comb %>% mutate(high_prop = FillDown(Var = high_prop)) %>%
+            as.data.frame
+
+## Merge in legislative violence variables
+comb <- merge(main_violence, comb, by = c('iso2c', 'year'), all = T) %>%
+        arrange(iso2c, year)
+
+# Convert NAs to missing
+for (i in c('violence', 'violence_y_cum')) comb[, i][is.na(comb[, i])] <- 0
+
+## Merge in polity
+comb <- merge(comb, polity, by = c('iso2c', 'year'), all = T) %>%
+    arrange(iso2c, year)
+
+## Merge wdi
+comb <- merge(comb, wdi, by = c('iso2c', 'year'), all.x = T) %>%
+    arrange(iso2c, year)
